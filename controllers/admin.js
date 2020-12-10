25c940083dbcb7ac8122584acd86904ea02f3972
Lex <alexay.pro@gmail.com>
2020-12-10 21:34:19 +0900

    mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const { populate } = require('../models/product');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -13,15 +14,17 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    req.user
-        .createProduct({
-            title: title,
-            price: price,
-            imageUrl: imageUrl,
-            description: description,
-        })
+    const product = new Product({
+        title: title,
+        price: price,
+        imageUrl: imageUrl,
+        description: description,
+        userId: req.user,
+    });
+    product
+        .save()
         .then((result) => {
-            console.log('Product Added!');
+            console.log('Product Created!');
             res.redirect('/admin/products');
         })
         .catch((err) => {
@@ -36,14 +39,11 @@ exports.getEditProduct = (req, res, next) => {
     }
 
     const prodId = req.params.productId;
-    req.user
-        .getProducts({ where: { id: prodId } })
-        .then((products) => {
-            const product = products[0];
+    Product.findById(prodId)
+        .then((product) => {
             if (!product) {
-                return res.redirect('/');
+                res.redirect('/');
             }
-
             res.render('admin/edit-product', {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
@@ -62,7 +62,8 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    Product.findByPk(prodId)
+
+    Product.findById(prodId)
         .then((product) => {
             product.title = updatedTitle;
             product.price = updatedPrice;
@@ -80,9 +81,11 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    req.user
-        .getProducts()
+    Product.find()
+        // .select('title price -_id')
+        // .populate('userId', 'name')
         .then((products) => {
+            console.log(products);
             res.render('admin/products', {
                 prods: products,
                 pageTitle: 'Admin Products',
@@ -96,12 +99,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByPk(prodId)
-        .then((product) => {
-            product.destroy();
-        })
+    Product.findByIdAndRemove(prodId)
         .then((result) => {
-            console.log('DESTROYED PRODUCT!');
+            console.log('PRODUCT REMOVED!');
             res.redirect('/admin/products');
         })
         .catch((err) => {
